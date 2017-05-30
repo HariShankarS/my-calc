f0aa6e336562d4ee08f2ca99c390ae81346c6738
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2017-05-30 14:04:48 +0530

    Factorial for Flaot values done

--- mycalc.js
+++ mycalc.js
@@ -406,11 +406,24 @@ $(document).ready(function() {
       return equation;
     }
 
-    function fact(number) {
-      if (number == 0) {
-        return 1;
-      }
-      return number * fact(number-1);
+    function fact(op) {
+     // Lanczos Approximation of the Gamma Function
+     // As described in Numerical Recipes in C (2nd ed. Cambridge University Press, 1992)
+     var z = op + 1;
+     var p = [1.000000000190015, 76.18009172947146, -86.50532032941677, 24.01409824083091, -1.231739572450155, 1.208650973866179E-3, -5.395239384953E-6];
+
+     var d1 = Math.sqrt(2 * Math.PI) / z;
+     var d2 = p[0];
+
+     for (var i = 1; i <= 6; ++i)
+      d2 += p[i] / (z + i);
+
+     var d3 = Math.pow((z + 5.5), (z + 0.5));
+     var d4 = Math.exp(-(z + 5.5));
+
+     d = d1 * d2 * d3 * d4;
+
+     return d;
     }
 
     function cube(number) {
